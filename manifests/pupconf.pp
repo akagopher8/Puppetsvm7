6f52ca597013353a537e098d74f0de56e5b19556
Jason Shortino <akagopher8@gmail.com>
2018-07-30 11:37:39 -0700

    Added network file

--- manifests/pupconf.pp
+++ manifests/pupconf.pp
@@ -20,11 +20,18 @@ node "svm7.llnl.gov" {
     show_diff => "true",
     source => '/etc/puppet/manifests/resolv.conf'
   }
-  file { 'etc/ssh/sshd_config':
+  file { '/etc/ssh/sshd_config':
     ensure => "file",
     owner  => "root",
     group  => "root",
     mode   => "600",
-    source => 'etc/puppet/mandifests/sshd_config'
+    source => 'etc/puppet/manifests/sshd_config'
+  }
+  file { '/etc/sysconfig/network':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => 'etc/puppet/manifests/network'
   }
 } # End node mynode.example.com
