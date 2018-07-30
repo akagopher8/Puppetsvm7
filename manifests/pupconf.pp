65375911f8099e949d1352afb99330d98fc38ca7
Jason Shortino <akagopher8@gmail.com>
2018-07-30 11:30:04 -0700

    Added the sshd_config file to puppet checks

--- manifests/pupconf.pp
+++ manifests/pupconf.pp
@@ -20,4 +20,11 @@ node "svm7.llnl.gov" {
     show_diff => "true",
     source => '/etc/puppet/manifests/resolv.conf'
   }
+  file { 'etc/ssh/sshd_config':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "600",
+    source => 'etc/puppet/mandifests/sshd_config'
+  }
 } # End node mynode.example.com
